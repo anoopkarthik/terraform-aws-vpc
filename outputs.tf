e35effa64413cff448cb6efe61e17131e6616295
Karthik <anoopkarthikaggala@gmail.com>
2024-10-10 21:31:00 +0530

    user profile

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,11 @@ output "database_subnet_ids"{
     value = aws_subnet.database[*].id
 }
 
+output "db_subnet_group_name"{
+    value = aws_db_subnet_group.default.name 
+}
+
+
 # output "az_info" {
 #     value = data.aws_availability_zones.available 
 # }
